28f039415420e4676ebeef131f5481c1a0f00634
Agasthya <mshyamala15@gmail.com>
2025-05-15 10:50:30 -0400

    Basic Variable code to read

---
+++ 02 VariableInfo.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+echo "Harish:: Hello Agasthya"
+echo "Agsthya:: Hi Harish"
+echo "Harish:: How are you doing today"
+echo "Agasthya:: Iam doing good!! Thanks for asking how about you Harish"
