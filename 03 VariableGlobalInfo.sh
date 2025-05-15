17f286368e5c082c9d5dbcbe1a96c27e19780967
Agasthya <mshyamala15@gmail.com>
2025-05-15 12:31:55 -0400

    Global Variable Decalred

---
+++ 03 VariableGlobalInfo.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+Person1=Harish
+Person2=Agasthya
+echo "$Pesron1:: Hello $Person2"
+echo "$Person2:: Hi $Person1"
+echo "$Person1:: How are you doing today"
+echo "$Person2:: I am doing good!! Thanks for asking how about you $Person1"
