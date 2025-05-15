2ab794d1ba5b07d711bf796ceb349e0da3c761b2
Agasthya <mshyamala15@gmail.com>
2025-05-15 15:28:12 -0400

    Multipilication

---
+++ 08 Multipication of two numbers.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+
+Timestamp =$(Date)
+
+echo "Time Stamp for entering the user inputs: $Timestamp"
+
+echo "Enter the Number1: "
+read$Number1
+echo "Enter the Number2: "
+read $Number2
+
+Mul =$((Number1*Number2))
+
+echo "Multipi :- $Mul"
