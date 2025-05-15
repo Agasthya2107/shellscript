2134e7b1cdce23345a8faa17606127e4c0a7c8e4
Agasthya <mshyamala15@gmail.com>
2025-05-15 15:59:23 -0400

    Values

--- 08 Multipication of two numbers.sh
+++ 08 Multipication of two numbers.sh
@@ -6,9 +6,11 @@ Time=$(Date)
 echo "Time Stamp for entering the user inputs: $Time"
 
 echo "Enter the Number1::"
-read $Number1
+read Number1
+echo "User Entered Number is : $Number1"
 echo "Enter the Number2::"
-read $Number2
+read Number2
+echo "Entered Secound Number : $Number2"
 
 Mul=$((Number1+Number2))
 echo "Multipi : $Mul"
